6230241cd81a7647490ee144886496afcb9f1443
KhanggNguyen <kukhang2411@gmail.com>
2019-11-05 16:57:52 +0100

    ajout tp2 tp3 hmin105

---
+++ HMIN105-Concurrence/TP2-FileDesMessages/client.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <pthread.h>
+
+#include "warn.h"
+#include "calc.h"
+
+int main(int argc, char**argv){
+    if(argc < 2){
+        printf("USAGE %s <FILE ID> \n", argv[0]);
+    }
+
+    int file_id = atoi(argv[1]);
+    calc_msg calc;
+    eval_msg eval;
+    calc.mtype = getpid();
+
+    while(1){
+        printf("Calcul : ");
+        scanf("%d %c %d", &calc.x, &calc.symb, &calc.y);
+        printf("Envoi de la requete : %d %c %d d'etiquette %ld dans la file d'id %d\n", calc.x, calc.symb, calc.y, calc.mtype, file_id);
+        int error;
+        error = msgsnd (file_id, &calc, sizeof(calc), 0); WARN_ERROR(error);
+        error = msgrcv (file_id, &eval, sizeof(eval), calc.mtype, 0);WARN_ERROR(error);
+        printf("Resultat : %d\n", eval.res);
+    
+    }
+    return 0;
+}
